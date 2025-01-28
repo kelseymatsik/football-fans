46d042e6b60f747227ff9b17f106fb5d27c08d5f
Kelsey Matsik <kelseymatsik@gmail.com>
2025-01-28 17:45:56 -0500

    add epoch function

--- Lab2.R
+++ Lab2.R
@@ -1,6 +1,7 @@
 possible_points<-c(-7,-3,3,7)
 possible_points<-c(-7,-3,3,7)
 
+
 rnd_pt_function<-function(down,ytg,fp){
   return(sample(possible_points,1))
 }
@@ -8,20 +9,24 @@ rnd_pt_function<-function(down,ytg,fp){
 rnd_pt_function()
 
 
-# Drive function
-
+# Drive Function
+## returns a state 
 drive <- function(down,ytg,fp){
-  location<-sample.int(120,1)
-  end <- c(1,10,location)
-  return(end)
+  next_fp <-sample.int(120,1)
+  next_state <- c(1,10,location)
+  return(next_state)
 }
-
 drive()
 
+# Initialize reference and opponent 
 reference <- 1 
 opponent <- -1 
 
+# Process_State Function 
+## takes in the state, adds points, and switches teams 
 proccess_state <- function(down,ytg,fp,team){
+  
+  
   score <- 0
   if (fp > 110) {
     score <- score + 3 
@@ -40,6 +45,49 @@ proccess_state <- function(down,ytg,fp,team){
   }
   
   return (c(added_points,next_team))
+}
+
+# Simulate_Epoch Function 
+## Computes the expected points during the epoch (from that state until the next score)
+simulate_epoch <- function(down, ytg, fp) {
+  score <- NA
+  team <- 1  # 1 for reference team, -1 for opponent
+  drive_count <- 0
+  max_drives <- 10 # Set max_drives as 10
+  
+  while (is.na(score) && drive_count < max_drives) {
+    state <- drive(down, ytg, fp)
+    down <- state[1]
+    ytg <- state[2]
+    fp <- state[3]
+    
+    result <- proccess_state(down, ytg, fp, team)
+    added_points <- result[1]
+    team <- result[2]
+    
+    # if the team scores 
+    if (added_points != 0) {
+      score <- added_points
+    }
+    
+    drive_count <- drive_count + 1
+  }
+  
+  # if the team doesn't score 
+  if (is.na(score)) {
+    score <- 0
+  }
+  
+  return(score)
+}
+
+# Top-Level Function
+simulate_game <- function(down, ytg, fp, n = 1000) {
+  results <- numeric(n)
+  for (i in 1:n) {
+    results[i] <- simulate_epoch(down, ytg, fp)
+  }
+  return(mean(results))
 }
 
 
