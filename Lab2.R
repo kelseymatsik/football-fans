67c401afda26d89bd637eacf44332aa251bd5fbe
Catherine-S1 <gmj6rx@virginia.edu>
2025-01-28 18:06:14 -0500

    minor syntax error fixed

--- Lab2.R
+++ Lab2.R
@@ -15,7 +15,7 @@ rnd_pt_function()
 ## Returns a state of the game
 drive <- function(down,ytg,fp){
   next_fp <-sample.int(120,1)
-  next_state <- c(1,10,location)
+  next_state <- c(1,10,next_fp)
   return(next_state)
 }
 drive()
