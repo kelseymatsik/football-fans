301c08c363a621030209f8ccee73310662ef5dc4
Catherine-S1 <gmj6rx@virginia.edu>
2025-02-04 18:09:48 -0500

    Fixed State Vector

--- Lab3.R
+++ Lab3.R
@@ -55,38 +55,16 @@ for (i in 1:10000) {
 }
 state_vector
 
-state_vector <- c(
-  ANA = 0.03219857, 
-  ARI = 0.03200487, 
-  ATL = 0.03445328, 
-  BAL = 0.03316139, 
-  BOS = 0.03584793,
-  CHA = 0.02930756, 
-  CHN = 0.0348977, 
-  CIN = 0.03064543, 
-  CLE = 0.03454301, 
-  COL = 0.0304538,
-  DET = 0.02959835, 
-  HOU = 0.03721329, 
-  KCA = 0.02999425, 
-  LAN = 0.03948072, 
-  MIA = 0.02969201,
-  MIL = 0.03498321, 
-  MIN = 0.03187847, 
-  NYA = 0.03830918, 
-  NYN = 0.03341753, 
-  OAK = 0.03197763,
-  PHI = 0.03243858, 
-  PIT = 0.03138499, 
-  SDN = 0.03235099, 
-  SEA = 0.03394242, 
-  SFN = 0.03400499,
-  SLN = 0.03536689, 
-  TBA = 0.0366062, 
-  TEX = 0.03184672, 
-  TOR = 0.03532205, 
-  WAS = 0.032678
-)
-sorted_state_vector <- sort(state_vector, decreasing = TRUE)
-sorted_state <- as.data.frame(sorted_state_vector)
-sorted_state
+# Adjust State Vector
+
+state_vectorT<-t(state_vector)
+
+state_vector_df<-data.frame(state_vectorT)
+
+library(tibble)
+
+state_vector_with_rownames <- rownames_to_column(state_vector_df, var = "RowID")
+
+#Sorted State Vector as a Data Frame
+
+sorted_state<-df_with_rownames[order(state_vector_with_rownames[, 2],decreasing=T), ]
